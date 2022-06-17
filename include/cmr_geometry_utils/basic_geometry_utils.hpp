58df07d28b029f5c2fce0a5c9105fb7847da4b00
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-06-17 14:20:33 +0200

    use const references for complex types

--- include/cmr_geometry_utils/basic_geometry_utils.hpp
+++ include/cmr_geometry_utils/basic_geometry_utils.hpp
@@ -7,13 +7,13 @@
 
 namespace cmr_geometry_utils {
 
-double compute_distance(geometry_msgs::msg::Point start, geometry_msgs::msg::Point goal)
+double compute_distance(const geometry_msgs::msg::Point & start, const geometry_msgs::msg::Point & goal)
 {
   return std::hypot(goal.x - start.x, 
                     goal.y - start.y);
 }
 
-double compute_angle_distance(geometry_msgs::msg::Quaternion start, geometry_msgs::msg::Quaternion goal)
+double compute_angle_distance(const geometry_msgs::msg::Quaternion & start, const geometry_msgs::msg::Quaternion & goal)
 {
   double start_yaw = angles::normalize_angle(tf2::getYaw(start));
   double goal_yaw = angles::normalize_angle(tf2::getYaw(goal));
