4307e40416cd10ac3bbd6443f11a29e2911c937a
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-06-21 14:08:14 +0200

    nested calls instead of redeclaration

--- include/cmr_geometry_utils/basic_geometry_utils.hpp
+++ include/cmr_geometry_utils/basic_geometry_utils.hpp
@@ -15,14 +15,12 @@ double compute_distance_2d(const geometry_msgs::msg::Point & start, const geomet
 
 double compute_distance_2d(const geometry_msgs::msg::Pose & start, const geometry_msgs::msg::Pose & goal)
 {
-  return std::hypot(goal.position.x - start.position.x, 
-                    goal.position.y - start.position.y);
+  return compute_distance_2d(start.position, goal.position);
 }
 
 double compute_distance_2d(const geometry_msgs::msg::PoseStamped & start, const geometry_msgs::msg::PoseStamped & goal)
 {
-  return std::hypot(goal.pose.position.x - start.pose.position.x, 
-                    goal.pose.position.y - start.pose.position.y);
+  return compute_distance_2d(start.pose, goal.pose);
 }
 
 
@@ -35,16 +33,12 @@ double compute_yaw_distance(const geometry_msgs::msg::Quaternion & start, const 
 
 double compute_yaw_distance(const geometry_msgs::msg::Pose & start, const geometry_msgs::msg::Pose & goal)
 {
-  double start_yaw = angles::normalize_angle(tf2::getYaw(start.orientation));
-  double goal_yaw = angles::normalize_angle(tf2::getYaw(goal.orientation));
-  return std::abs(angles::normalize_angle(goal_yaw - start_yaw));
+  return compute_yaw_distance(start.orientation, goal.orientation);
 }
 
 double compute_yaw_distance(const geometry_msgs::msg::PoseStamped & start, const geometry_msgs::msg::PoseStamped & goal)
 {
-  double start_yaw = angles::normalize_angle(tf2::getYaw(start.pose.orientation));
-  double goal_yaw = angles::normalize_angle(tf2::getYaw(goal.pose.orientation));
-  return std::abs(angles::normalize_angle(goal_yaw - start_yaw));
+  return compute_yaw_distance(start.pose, goal.pose);
 }
 
 
