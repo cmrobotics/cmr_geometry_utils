b5fc4ceb980b5df6facdb8c7f72866ad42c9ea2d
Arek Nowakowski <arekin19@gmail.com>
2022-08-31 17:27:56 +0200

    add signed distance for angles

--- include/cmr_geometry_utils/basic_geometry_utils.hpp
+++ include/cmr_geometry_utils/basic_geometry_utils.hpp
@@ -23,7 +23,6 @@ inline double compute_distance_2d(const geometry_msgs::msg::PoseStamped & start,
   return compute_distance_2d(start.pose, goal.pose);
 }
 
-
 inline double compute_yaw_distance(const geometry_msgs::msg::Quaternion & start, const geometry_msgs::msg::Quaternion & goal)
 {
   double start_yaw = tf2::getYaw(start);
@@ -41,5 +40,21 @@ inline double compute_yaw_distance(const geometry_msgs::msg::PoseStamped & start
   return compute_yaw_distance(start.pose, goal.pose);
 }
 
+inline double compute_signed_yaw_distance(const geometry_msgs::msg::Quaternion & start, const geometry_msgs::msg::Quaternion & goal)
+{
+  double start_yaw = tf2::getYaw(start);
+  double goal_yaw = tf2::getYaw(goal);
+  return angles::normalize_angle(goal_yaw - start_yaw);
+}
+
+inline double compute_signed_yaw_distance(const geometry_msgs::msg::Pose & start, const geometry_msgs::msg::Pose & goal)
+{
+  return compute_signed_yaw_distance(start.orientation, goal.orientation);
+}
+
+inline double compute_signed_yaw_distance(const geometry_msgs::msg::PoseStamped & start, const geometry_msgs::msg::PoseStamped & goal)
+{
+  return compute_signed_yaw_distance(start.pose, goal.pose);
+}
 
 }
