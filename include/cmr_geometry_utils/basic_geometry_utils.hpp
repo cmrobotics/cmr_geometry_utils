6f25e9a566847863fd3a66a4e375cede41b81c3c
Arek Nowakowski <arekin19@gmail.com>
2022-08-31 17:38:09 +0200

    avoid duplicates

--- include/cmr_geometry_utils/basic_geometry_utils.hpp
+++ include/cmr_geometry_utils/basic_geometry_utils.hpp
@@ -23,23 +23,6 @@ inline double compute_distance_2d(const geometry_msgs::msg::PoseStamped & start,
   return compute_distance_2d(start.pose, goal.pose);
 }
 
-inline double compute_yaw_distance(const geometry_msgs::msg::Quaternion & start, const geometry_msgs::msg::Quaternion & goal)
-{
-  double start_yaw = tf2::getYaw(start);
-  double goal_yaw = tf2::getYaw(goal);
-  return std::abs(angles::normalize_angle(goal_yaw - start_yaw));
-}
-
-inline double compute_yaw_distance(const geometry_msgs::msg::Pose & start, const geometry_msgs::msg::Pose & goal)
-{
-  return compute_yaw_distance(start.orientation, goal.orientation);
-}
-
-inline double compute_yaw_distance(const geometry_msgs::msg::PoseStamped & start, const geometry_msgs::msg::PoseStamped & goal)
-{
-  return compute_yaw_distance(start.pose, goal.pose);
-}
-
 inline double compute_signed_yaw_distance(const geometry_msgs::msg::Quaternion & start, const geometry_msgs::msg::Quaternion & goal)
 {
   double start_yaw = tf2::getYaw(start);
@@ -55,6 +38,21 @@ inline double compute_signed_yaw_distance(const geometry_msgs::msg::Pose & start
 inline double compute_signed_yaw_distance(const geometry_msgs::msg::PoseStamped & start, const geometry_msgs::msg::PoseStamped & goal)
 {
   return compute_signed_yaw_distance(start.pose, goal.pose);
+}
+
+inline double compute_yaw_distance(const geometry_msgs::msg::Quaternion & start, const geometry_msgs::msg::Quaternion & goal)
+{
+  return std::abs(compute_signed_yaw_distance(start, goal));
+}
+
+inline double compute_yaw_distance(const geometry_msgs::msg::Pose & start, const geometry_msgs::msg::Pose & goal)
+{
+  return compute_yaw_distance(start.orientation, goal.orientation);
+}
+
+inline double compute_yaw_distance(const geometry_msgs::msg::PoseStamped & start, const geometry_msgs::msg::PoseStamped & goal)
+{
+  return compute_yaw_distance(start.pose, goal.pose);
 }
 
 }
