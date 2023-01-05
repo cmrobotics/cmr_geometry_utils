66fce93c59b0dc7b65d1aa28375935e0bf775475
Aaron <aaron.lipinski@cm-robotics.com>
2023-01-05 18:23:05 +0100

    prepare for another set of tests

--- test/pose_geometry_test.cpp
+++ test/pose_geometry_test.cpp
@@ -5,8 +5,6 @@
 
 TEST(PoseGeometryTest, calculate_average_pose)
 {
-    geometry_msgs::msg::Pose p;
-
     geometry_msgs::msg::Pose p1;
     p1.position.x = 0.0;
     p1.position.y = 0.0;
@@ -27,17 +25,23 @@ TEST(PoseGeometryTest, calculate_average_pose)
     p2.orientation.z = 0.0;
     p2.orientation.w = 0.4771588;
 
+    // test using std::vector
+    {
+      geometry_msgs::msg::Pose p;
+      ASSERT_EQ(p.position.x, 0);
+      ASSERT_EQ(p.position.y, 0);
+      ASSERT_EQ(p.position.z, 0);
 
+      std::vector<geometry_msgs::msg::Pose> v = {p1, p2};
+      cmr_geometry_utils::pose::average(v, p);
 
-    std::vector<geometry_msgs::msg::Pose> v = {p1, p2};
-    cmr_geometry_utils::pose::average(v, p);
+      ASSERT_EQ(p.position.x, 10.);
+      ASSERT_EQ(p.position.y, 5.);
+      ASSERT_EQ(p.position.z, 0.0);
 
-    ASSERT_EQ(p.position.x, 10.);
-    ASSERT_EQ(p.position.y, 5.);
-    ASSERT_EQ(p.position.z, 0.0);
-    
-    ASSERT_NEAR(p.orientation.x, 0.5112, 1e-4);
-    ASSERT_EQ(p.orientation.y, 0.0);
-    ASSERT_EQ(p.orientation.z, 0.0);
-    ASSERT_NEAR(p.orientation.w, 0.8594, 1e-4);
+      ASSERT_NEAR(p.orientation.x, 0.5112, 1e-4);
+      ASSERT_EQ(p.orientation.y, 0.0);
+      ASSERT_EQ(p.orientation.z, 0.0);
+      ASSERT_NEAR(p.orientation.w, 0.8594, 1e-4);
+    }
 }
